a6e77a762bd781b0d8ef5d8899368c42f5d981f2
micsarawut <65011212206@msu.ac.th>
2024-09-25 01:34:37 +0700

    ss

--- lib/pages/send_final.dart
+++ lib/pages/send_final.dart
@@ -5,9 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:get_storage/get_storage.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fontend_miniproject2/config/config.dart';
 import 'package:fontend_miniproject2/pages/home_user.dart';
 import 'package:fontend_miniproject2/models/get_user2.dart';
+import 'package:fontend_miniproject2/models/get_product.dart';
 import 'package:fontend_miniproject2/models/add_user_request.dart';
 
 class SendFinalPage extends StatefulWidget {
@@ -31,6 +33,7 @@ class SendFinalPage extends StatefulWidget {
 class _SendFinalPageState extends State<SendFinalPage> {
   late Future<void> loadData_User;
   List<GetUser2> data = [];
+  late GetProduct pro;
 
   @override
   void initState() {
@@ -59,145 +62,146 @@ class _SendFinalPageState extends State<SendFinalPage> {
         ),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            FutureBuilder(
-              future: loadData_User,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                }
-                return SingleChildScrollView(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border:
-                          Border.all(color: Colors.grey, width: 2), // ขอบกรอบ
-                      borderRadius: BorderRadius.circular(8), // มุมโค้ง
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FutureBuilder(
+                future: loadData_User,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  }
+                  return SingleChildScrollView(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border:
+                            Border.all(color: Colors.grey, width: 2), // ขอบกรอบ
+                        borderRadius: BorderRadius.circular(8), // มุมโค้ง
+                      ),
+                      padding: const EdgeInsets.all(10), // ระยะห่างภายใน
+                      child: Column(
+                        children: [
+                          if (data.isNotEmpty)
+                            Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      'ผู้ส่ง: ${data[0].name} ${data[0].lastname}',
+                                      style: const TextStyle(fontSize: 15),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      '${data[0].phone}',
+                                      style: const TextStyle(
+                                          fontSize: 15, color: Colors.black54),
+                                    ),
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      '${data[0].address}',
+                                      style: const TextStyle(
+                                          fontSize: 15, color: Colors.black54),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 10),
+                                // เพิ่มเส้นขั้นที่นี่
+                                const Divider(
+                                    height: 30,
+                                    thickness: 1,
+                                    color: Colors.grey), // เส้นขั้น
+                              ],
+                            ),
+                          if (data.length > 1)
+                            Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      'ผู้รับ: ${data[1].name} ${data[1].lastname}',
+                                      style: const TextStyle(fontSize: 15),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      '${data[1].phone}',
+                                      style: const TextStyle(
+                                          fontSize: 15, color: Colors.black54),
+                                    ),
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      '${data[1].address}',
+                                      style: const TextStyle(
+                                          fontSize: 15, color: Colors.black54),
+                                      softWrap: true,
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                        ],
+                      ),
                     ),
-                    padding: const EdgeInsets.all(10), // ระยะห่างภายใน
-                    child: Column(
-                      children: [
-                        if (data.isNotEmpty)
-                          Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Text(
-                                    'ผู้ส่ง: ${data[0].name} ${data[0].lastname}',
-                                    style: const TextStyle(fontSize: 15),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Text(
-                                    '${data[0].phone}',
-                                    style: const TextStyle(
-                                        fontSize: 15, color: Colors.black54),
-                                  ),
-                                ],
-                              ),
-                              Column(
-                                children: [
-                                  Text(
-                                    '${data[0].address}',
-                                    style: const TextStyle(
-                                        fontSize: 15, color: Colors.black54),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(height: 10),
-                              // เพิ่มเส้นขั้นที่นี่
-                              const Divider(
-                                  height: 30,
-                                  thickness: 1,
-                                  color: Colors.grey), // เส้นขั้น
-                            ],
-                          ),
-                        if (data.length > 1)
-                          Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Text(
-                                    'ผู้รับ: ${data[1].name} ${data[1].lastname}',
-                                    style: const TextStyle(fontSize: 15),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Text(
-                                    '${data[1].phone}',
-                                    style: const TextStyle(
-                                        fontSize: 15, color: Colors.black54),
-                                  ),
-                                ],
-                              ),
-                              Column(
-                                children: [
-                                  Text(
-                                    '${data[1].address}',
-                                    style: const TextStyle(
-                                        fontSize: 15, color: Colors.black54),
-                                    softWrap: true,
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                      ],
+                  );
+                },
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                child: Text(
+                  "รายการพัสดุ",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 2), // ขอบกรอบ
+                  borderRadius: BorderRadius.circular(8), // มุมโค้ง
+                ),
+                padding: const EdgeInsets.fromLTRB(10, 10, 150, 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 10),
+                    Text(
+                      'ชื่อพัสดุ: ${widget.nameProduct}',
+                      style: const TextStyle(fontSize: 15),
                     ),
-                  ),
-                );
-              },
-            ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
-              child: Text(
-                "รายการพัสดุ",
-                style:
-                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    Text(
+                      'รายละเอียด: ${widget.detailProduct}',
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                    const SizedBox(height: 10),
+                    widget.image != null
+                        ? Image.file(
+                            File(widget.image!.path),
+                            width: 200,
+                            height: 200,
+                            fit: BoxFit.cover,
+                          )
+                        : const Text('ไม่มีรูปภาพ'),
+                    const SizedBox(height: 20),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey, width: 2), // ขอบกรอบ
-                borderRadius: BorderRadius.circular(8), // มุมโค้ง
-              ),
-              padding: const EdgeInsets.fromLTRB(10, 10, 150, 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 10),
-                  Text(
-                    'ชื่อพัสดุ: ${widget.nameProduct}',
-                    style: const TextStyle(fontSize: 15),
-                  ),
-                  Text(
-                    'รายละเอียด: ${widget.detailProduct}',
-                    style: const TextStyle(fontSize: 15),
-                  ),
-                  const SizedBox(height: 10),
-                  widget.image != null
-                      ? Image.file(
-                          File(widget.image!.path),
-                          width: 200,
-                          height: 200,
-                          fit: BoxFit.cover,
-                        )
-                      : const Text('ไม่มีรูปภาพ'),
-                  const SizedBox(height: 20),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomAppBar(
@@ -207,12 +211,13 @@ class _SendFinalPageState extends State<SendFinalPage> {
             FilledButton(
                 style: FilledButton.styleFrom(
                     backgroundColor: const Color.fromARGB(255, 72, 0, 0),
-                    padding: const EdgeInsets.symmetric(horizontal: 150, vertical: 10),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 140, vertical: 10),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(5),
                     )),
                 onPressed: addProduct,
-                child: const Text("ถัดไป"))
+                child: const Text("ทำการจัดส่ง"))
           ],
         ),
       ),
@@ -281,16 +286,7 @@ class _SendFinalPageState extends State<SendFinalPage> {
                       ),
                       elevation: 5,
                     ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => HomeUserPage(
-                            uid: widget.uid,
-                          ),
-                        ),
-                      );
-                    },
+                    onPressed: loadPro,
                   ),
                 ],
               ),
@@ -302,5 +298,40 @@ class _SendFinalPageState extends State<SendFinalPage> {
       // จัดการกรณีที่การทำงานไม่สำเร็จ (สามารถเพิ่มได้ถ้าต้องการ)
       log('Error: ${response.statusCode}');
     }
+  }
+
+  Future<void> loadPro() async {
+    var config = await Configuration.getConfig();
+    var url = config['apiEndpoint'];
+
+    final response = await http.get(Uri.parse("$url/product/get-latest"));
+    log('Requesting URL: $url');
+    log(response.body);
+    var jsonResponse = (response.body);
+    pro = getProductFromJson(jsonResponse);
+
+    var db = FirebaseFirestore.instance;
+
+    var data = {
+      'pro_name': pro.proName,
+      'pro_detail': pro.proDetail,
+      'proimg': pro.proImg,
+      'pro_status': pro.proStatus,
+      'traking_number': pro.trackingNumber,
+      'uid_fk_send': pro.uidFkSend,
+      'uid_fk_accept': pro.uidFkAccept,
+      'rid_fk': pro.ridFk
+    };
+
+    db.collection('inbox').doc(pro.pid.toString()).set(data);
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => HomeUserPage(
+          uid: widget.uid,
+        ),
+      ),
+    );
   }
 }
